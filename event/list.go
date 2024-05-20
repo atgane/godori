66da54a519c84ab747558655c3fa0d53d8a90e37
atgane <hyper201286@gmail.com>
2024-05-20 23:10:14 +0900

    comment list + table

--- event/list.go
+++ event/list.go
@@ -5,11 +5,13 @@ import (
 	"sync"
 )
 
+// List is a thread-safe list structure.
 type List[T any] struct {
-	list []T
-	mu   sync.RWMutex
+	list []T          // Slice to store list elements.
+	mu   sync.RWMutex // Read-write mutex to protect the list.
 }
 
+// NewList creates a new List with an initial size.
 func NewList[T any](initSize int) *List[T] {
 	li := &List[T]{}
 	li.list = make([]T, initSize)
@@ -17,40 +19,43 @@ func NewList[T any](initSize int) *List[T] {
 	return li
 }
 
+// Broadcast applies the handler function to each element in the list.
 func (li *List[T]) Broadcast(handler func(T)) {
-	li.mu.RLock()
-	defer li.mu.RUnlock()
+	li.mu.RLock()         // Acquire read lock.
+	defer li.mu.RUnlock() // Release read lock when function exits.
 
 	for _, e := range li.list {
 		handler(e)
 	}
 }
 
+// BroadcastParallel applies the handler function to each element in the list in parallel.
 func (li *List[T]) BroadcastParallel(handler func(T), maxParallel int) {
-	li.mu.RLock()
-	defer li.mu.RUnlock()
+	li.mu.RLock()         // Acquire read lock.
+	defer li.mu.RUnlock() // Release read lock when function exits.
 
 	if maxParallel == 0 {
-		maxParallel = len(li.list)
+		maxParallel = len(li.list) // Set maxParallel to size of list if it's 0.
 	}
 
-	waitCh := make(chan struct{}, maxParallel)
+	waitCh := make(chan struct{}, maxParallel) // Channel to control the number of parallel executions.
 	wg := &sync.WaitGroup{}
 	for _, e := range li.list {
 		wg.Add(1)
 		waitCh <- struct{}{}
 		go func(e T) {
 			handler(e)
-			<-waitCh
-			wg.Done()
+			<-waitCh  // Signal that one handler execution is done.
+			wg.Done() // Decrement the wait group counter.
 		}(e)
 	}
-	wg.Wait()
+	wg.Wait() // Wait for all handler executions to complete.
 }
 
+// Unicast applies the handler function to the element identified by the key.
 func (li *List[T]) Unicast(key int, handler func(T)) error {
-	li.mu.RLock()
-	defer li.mu.RUnlock()
+	li.mu.RLock()         // Acquire read lock.
+	defer li.mu.RUnlock() // Release read lock when function exits.
 
 	if key >= len(li.list) {
 		return errors.New("key exceeds list size")
@@ -61,14 +66,19 @@ func (li *List[T]) Unicast(key int, handler func(T)) error {
 	return nil
 }
 
+// Insert adds a new element to the end of the list.
 func (li *List[T]) Insert(e T) {
-	li.mu.Lock()
-	defer li.mu.Unlock()
+	li.mu.Lock()         // Acquire write lock.
+	defer li.mu.Unlock() // Release write lock when function exits.
 
 	li.list = append(li.list, e)
 }
 
+// Update modifies the element at the specified index.
 func (li *List[T]) Update(key int, e T) error {
+	li.mu.Lock()         // Acquire write lock.
+	defer li.mu.Unlock() // Release write lock when function exits.
+
 	if key >= len(li.list) {
 		return errors.New("key exceeds list size")
 	}
@@ -78,12 +88,16 @@ func (li *List[T]) Update(key int, e T) error {
 	return nil
 }
 
-func (li *List[T]) Delete(key int, e T) error {
+// Delete removes the element at the specified index.
+func (li *List[T]) Delete(key int) error {
+	li.mu.Lock()         // Acquire write lock.
+	defer li.mu.Unlock() // Release write lock when function exits.
+
 	if key >= len(li.list) {
 		return errors.New("key exceeds list size")
 	}
 
-	li.list = append(li.list[:key], li.list[:key+1]...)
+	li.list = append(li.list[:key], li.list[key+1:]...)
 
 	return nil
 }
--- event/table.go
+++ event/table.go
@@ -5,11 +5,13 @@ import (
 	"sync"
 )
 
+// Table is a thread-safe map structure.
 type Table[F comparable, T any] struct {
-	table map[F]T
-	mu    sync.RWMutex
+	table map[F]T      // Underlying map to store key-value pairs.
+	mu    sync.RWMutex // Read-write mutex to protect the map.
 }
 
+// NewTable creates a new Table with an initial size.
 func NewTable[F comparable, T any](initSize int) *Table[F, T] {
 	t := &Table[F, T]{}
 	t.table = make(map[F]T, initSize)
@@ -17,40 +19,43 @@ func NewTable[F comparable, T any](initSize int) *Table[F, T] {
 	return t
 }
 
+// Broadcast applies the handler function to each element in the table.
 func (t *Table[F, T]) Broadcast(handler func(T)) {
-	t.mu.RLock()
-	defer t.mu.RUnlock()
+	t.mu.RLock()         // Acquire read lock.
+	defer t.mu.RUnlock() // Release read lock when function exits.
 
 	for _, e := range t.table {
 		handler(e)
 	}
 }
 
+// BroadcastParallel applies the handler function to each element in the table in parallel.
 func (t *Table[F, T]) BroadcastParallel(handler func(T), maxParallel int) {
-	t.mu.RLock()
-	defer t.mu.RUnlock()
+	t.mu.RLock()         // Acquire read lock.
+	defer t.mu.RUnlock() // Release read lock when function exits.
 
 	if maxParallel == 0 {
-		maxParallel = len(t.table)
+		maxParallel = len(t.table) // Set maxParallel to size of table if it's 0.
 	}
 
-	waitCh := make(chan struct{}, maxParallel)
+	waitCh := make(chan struct{}, maxParallel) // Channel to control the number of parallel executions.
 	wg := &sync.WaitGroup{}
 	for _, e := range t.table {
 		wg.Add(1)
 		waitCh <- struct{}{}
 		go func(e T) {
 			handler(e)
-			<-waitCh
-			wg.Done()
+			<-waitCh  // Signal that one handler execution is done.
+			wg.Done() // Decrement the wait group counter.
 		}(e)
 	}
-	wg.Wait()
+	wg.Wait() // Wait for all handler executions to complete.
 }
 
+// Unicast applies the handler function to the element identified by the key.
 func (t *Table[F, T]) Unicast(key F, handler func(T)) error {
-	t.mu.RLock()
-	defer t.mu.RUnlock()
+	t.mu.RLock()         // Acquire read lock.
+	defer t.mu.RUnlock() // Release read lock when function exits.
 
 	e, exist := t.table[key]
 	if !exist {
@@ -62,23 +67,40 @@ func (t *Table[F, T]) Unicast(key F, handler func(T)) error {
 	return nil
 }
 
+// LoadOrStore returns the existing value for the key if present, otherwise stores and returns the given value.
+func (t *Table[F, T]) LoadOrStore(key F, e T) (val T, load bool) {
+	t.mu.Lock()         // Acquire write lock.
+	defer t.mu.Unlock() // Release write lock when function exits.
+
+	v, exist := t.table[key]
+	if !exist {
+		t.table[key] = e
+		return e, false
+	}
+
+	return v, true
+}
+
+// Upsert inserts or updates the value for the given key.
 func (t *Table[F, T]) Upsert(key F, e T) {
-	t.mu.Lock()
-	defer t.mu.Unlock()
+	t.mu.Lock()         // Acquire write lock.
+	defer t.mu.Unlock() // Release write lock when function exits.
 
 	t.table[key] = e
 }
 
+// Delete removes the element identified by the key.
 func (t *Table[F, T]) Delete(key F) {
-	t.mu.Lock()
-	defer t.mu.Unlock()
+	t.mu.Lock()         // Acquire write lock.
+	defer t.mu.Unlock() // Release write lock when function exits.
 
 	delete(t.table, key)
 }
 
+// Len returns the number of elements in the table.
 func (t *Table[F, T]) Len() int {
-	t.mu.RLock()
-	defer t.mu.Unlock()
+	t.mu.RLock()         // Acquire read lock.
+	defer t.mu.RUnlock() // Release read lock when function exits.
 
 	return len(t.table)
 }
